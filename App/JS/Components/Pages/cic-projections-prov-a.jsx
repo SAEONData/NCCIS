af56a5055728d120a83ea34f71a43af8f23e7c01
Taryn <taryn@saeon.ac.za>
2019-07-22 18:07:34 +0200

    map additions
    map complete /cic-projections-provincial

--- App/JS/Components/Pages/cic-projections-prov-a.jsx
+++ App/JS/Components/Pages/cic-projections-prov-a.jsx
@@ -8,7 +8,7 @@ render() {
     return (
       <>
       <div className="ea-tabs">
-        <div className="tab-parent ZA-NC">
+        <div className="tab-parent" maploc="ZA-NC">
           <h3>Northern Cape:</h3>
           <div className="tab-child">
           <p>The Northern Cape is the driest province in South Africa, with some areas receiving less than 70 mm/year on average. The north western parts of the province are arid and experience very infrequent rainfall events produced either by the passage of rare cold fronts in winter or occasional convective rainfall during summer. The south western parts experience infrequent winter rainfall, while the eastern two thirds of the province experience summer rainfall associated with local or large scale convective rainfall systems. The south and west of the province experience low rainfall during winter. The eastern part of the province responds partly to ENSO oscillations while the south and western areas having very weak, if any, association with ENSO.</p>
@@ -25,7 +25,7 @@ render() {
           </div>
         </div>
 
-        <div className="tab-parent ZA-WC">
+        <div className="tab-parent" maploc="ZA-WC">
           <h3>Western Cape:</h3>
           <div className="tab-child">
           <p>The Western Cape, dominated by ocean, mountains and plains (coastal and inland), has a very diverse climate. Locations such as Bethlehem in the Hottentots Holland mountains experience some of the highest annual rainfall totals in the country (1500 mm/year), whereas Laingsberg only receives around 120 mm/year. Temperatures also range widely from cool coastal mountains often covered by low orographic cloud where even in summer temperature rarely exceed 25 °C, through to semi-arid Karoo valleys where summer temperatures can average 35 'C.</p>
@@ -44,7 +44,7 @@ render() {
         </div>
 
         
-        <div className="tab-parent ZA-EC">
+        <div className="tab-parent" maploc="ZA-EC">
           <h3>Eastern Cape:</h3>
           <div className="tab-child">
           <p>The Eastern Cape, like the Western Cape, is characterized by a very diverse climate due to the proximity to the ocean and extensive mountain ranges and altitude variations. Minimum temperatures in Barkley East (1800 m) hover around 0 °C during the winter months, with snow not an unusual occurrence. Port St Johns on the other hand experienced average winter temperatures more than 10 °C warmer. The significant east-west mountain ranges produce stark rainfall climate gradients. Willomore receives around 240 mm/year with mostly summer rainfall, whereas Tsitsikamma, a mere 120 km south, receives around 720 mm/year of all year around rainfall.</p>
@@ -59,7 +59,7 @@ render() {
           </div>
         </div>
         
-        <div className="tab-parent ZA-NL">
+        <div className="tab-parent" maploc="ZA-NL">
           <h3>KwaZulu-Natal:</h3>
           <div className="tab-child">
           <p>KwaZulu-Natal is the wettest province in South Africa, with high rainfall totals occurring along both the eastern escarpment and over the coastal areas. It is a summer rainfall region that experiences hot and humid summers and mild winters.</p>
@@ -70,7 +70,7 @@ render() {
           </div>
         </div>
         
-        <div className="tab-parent ZA-MP">
+        <div className="tab-parent" maploc="ZA-MP">
           <h3>Mpumalanga:</h3>
           <div className="tab-child">
           <p>Mpumalanga may plausibly experience a climate future that is significantly hotter and drier compared to the present-day climate. Under low mitigation, temperature increases as large as 3 °C may occur by 2040-2060, with associated drastic decreases in rainfall. Such a climate regime will also be associated with an increase in the frequency of occurrence of heat-wave days and high fire-danger days. Such a change towards a generally warmer and drier climate would pose significant threats to the forestry sector, due to the likelihood for more frequent forest fires occurring during more frequent periods of drought.</p>
@@ -79,7 +79,7 @@ render() {
           </div>
         </div>
         
-        <div className="tab-parent ZA-LP">
+        <div className="tab-parent" maploc="ZA-LP">
           <h3>Limpopo:</h3>
           <div className="tab-child">
           <p>Limpopo is a summer rainfall region that generally experiences hot summers and cooler winters. Rainfall varies greatly across the province, with large parts of the Limpopo basin being semi-arid, and with high rainfall and rainforests along the eastern escarpment of the province. The province sporadically suffer from devastating flood events, when tropical lows or cyclones from the Indian Ocean make landfall over neigbouring Mozambique, or even over Limpopo itself.</p>
@@ -90,7 +90,7 @@ render() {
           </div>
         </div>
         
-        <div className="tab-parent ZA-GT">
+        <div className="tab-parent" maploc="ZA-GT">
           <h3>Gauteng:</h3>
           <div className="tab-child">
           <p>The Gauteng Province is the economic heartland of South Africa. The province falls in the summer rainfall region, and receives the bulk of its rainfall in the form of thunderstorms. Annual rainfall totals reach 700 mm over much of the province. Winters over Gauteng are dry and associated with clear skies, cold nights that occur in association with the formation of strong inversion layers and polluted mornings. It is critical realise that Gauteng’s water security does not only depend on local rainfall and streamflow and dams located within the province, but that about 40% of Gauteng’s rainfall is provided by the mega-deam region of southeastern South Africa.</p>
@@ -101,7 +101,7 @@ render() {
           </div>
         </div>
         
-        <div className="tab-parent ZA-FS">
+        <div className="tab-parent" maploc="ZA-FS">
           <h3>Free State:</h3>
           <div className="tab-child">
           <p>The Free State spans the centre of the country, and ranges from the relatively dry, hot and arid west (Bloemhof receives around 450 mm/year) and south through to higher altitude cooler wetter climate in the north and east (Royal National Park receives 1200 mm/year). It is entirely a summer rainfall region, with rainfall resulting from either local small scale convective events or larger organised convection. Rainfall in the province is partly linked to ENSO, though it is not uncommon for El-Nino years to normal or even wetter than normal in a few cases. The province is the “bread basket” of the country with extensive maize, wheat, and other crop production. This is a result of generally good soils and moderate climate, with sufficient rainfall to allow rain-fed agriculture.
@@ -117,7 +117,7 @@ render() {
           </div>
         </div>
         
-        <div className="tab-parent ZA-NW">
+        <div className="tab-parent" maploc="ZA-NW">
           <h3>North West:</h3>
           <div className="tab-child">
           <h5>Narrative 1: A hot and dry future</h5>
--- App/JS/custom.js
+++ App/JS/custom.js
@@ -21,15 +21,19 @@ $('body').on('click', '.ea-parent span', function(){
 $('body').on('click', '.tab-parent h3', function(){
     var ea_tab_parent = $(this).parent('.tab-parent');
     var ea_tab_child = $(this).next('.tab-child');
+    var map_path = $(ea_tab_parent).attr('maploc');
+    $('#svg2 path.active-path').removeClass('active-path');
+    $('#'+map_path).addClass('active-path');
     $(ea_tab_child).slideToggle(300);
     $('.tab-parent ').removeClass('active-tab');
     $(ea_tab_parent).addClass('active-tab');
+
 });
 $('body').on('click', '#svg2 path', function(){
     $('#svg2 path.active-path').removeClass('active-path');
     $(this).addClass('active-path');
     var path_id = $(this).attr('id');
-    var parent_info = $('.ea-tabs').find('.' + path_id);
+    var parent_info = $('.ea-tabs').find('[maploc="'+ path_id+'"');
     $('.tab-parent ').removeClass('active-tab');
     $('.tab-child').slideUp(300);
     $(parent_info).addClass('active-tab').find('.tab-child').slideDown(300);
